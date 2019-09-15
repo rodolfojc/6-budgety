f3c1bb64d788113c95b5897727d1607366f39120
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-09-15 20:15:55 +0100

    Function Constructors and Data Structures

--- starter/app.js
+++ starter/app.js
@@ -3,6 +3,32 @@
 
 var budgetController = (function(){
     
+    // FUNCTION CONSTRUCTOR - EXPENSE
+    var Expense = function (id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    // FUNCTION CONSTRUCTOR - INCOME
+    var Income = function (id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    // DATA STRUCTURE
+    var data = {
+        allItems: {
+            exp: [],
+            inc: [],
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+        
+    }
         
 })();
 
