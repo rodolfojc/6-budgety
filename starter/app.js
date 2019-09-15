f917194126aee78dbd1590bad9547a2f74dbdbca
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-09-15 19:32:04 +0100

    Click and Keypress Events

--- starter/app.js
+++ starter/app.js
@@ -1,36 +1,52 @@
 
+// BUDGET CONTROLLER
 
 var budgetController = (function(){
     
-    var x = 23;
-    
-    var add = function(a) {
-        return x + a;
-    }
-    
-    return {
-        publicTest : function(b){
-            console.log(add(b))
-        }
-    }
-    
+        
 })();
 
+// USER INTERFACE CONTROLLER
 var UIController = (function(){
     
     // MODULE
     
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetContr, UIContr){
     
-    // MODULE
-    var z = budgetContr.publicTest(5);
+// MODULE
+
+var controlAddItem = function(){
     
-    return {
-        anotherPublic : function(){
-        console.log(z);
-        }
+    console.log('Enter was pressed!!');
+    
+    // 1.- GET THE DATA
+    
+    // 2.- ADD THE ITEM TO THE BUDGET CONTROLLER
+    
+    // 3.- ADD THE ITEM TO THE USER INTERFACE
+    
+    // 4.- CALCULATE THE BUDGET
+    
+    // 5.- DISPLAY THE BUDGET
+    
+}    
+
+document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+
+// GLOBAL DOCUMENT
+    
+document.addEventListener('keypress', function(event){
+    
+    if(event.keyCode === 13 || event.which === 13) {
+        controlAddItem();
+        
     }
+    
+    
+});
+    
     
 })(budgetController, UIController);
