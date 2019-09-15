7d3a39b8a62a883d2b1e8634b7770ecb8f80772a
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-09-15 19:56:03 +0100

    Getting Data - Inputs en Button Section

--- starter/app.js
+++ starter/app.js
@@ -9,7 +9,28 @@ var budgetController = (function(){
 // USER INTERFACE CONTROLLER
 var UIController = (function(){
     
-    // MODULE
+// MODULE
+    
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+    
+    return {
+      getInput : function(){
+          return {
+              type: document.querySelector(DOMStrings.inputType).value, // INC OR EXP
+              description: document.querySelector(DOMStrings.inputDescription).value,
+              value : document.querySelector(DOMStrings.inputValue).value
+          };          
+      },
+        
+      getDOMString : function(){
+          return DOMStrings;
+      }
+    };
     
 })();
 
@@ -17,33 +38,35 @@ var UIController = (function(){
 var controller = (function(budgetContr, UIContr){
     
 // MODULE
+    
+    var DOM = UIContr.getDOMString();
 
-var controlAddItem = function(){
-    
-    console.log('Enter was pressed!!');
-    
-    // 1.- GET THE DATA
-    
-    // 2.- ADD THE ITEM TO THE BUDGET CONTROLLER
-    
-    // 3.- ADD THE ITEM TO THE USER INTERFACE
-    
-    // 4.- CALCULATE THE BUDGET
-    
-    // 5.- DISPLAY THE BUDGET
-    
-}    
+    var controlAddItem = function(){
 
-document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+        // 1.- GET THE DATA
+        var input = UIContr.getInput();
+        console.log(input);
 
-// GLOBAL DOCUMENT
+        // 2.- ADD THE ITEM TO THE BUDGET CONTROLLER
+
+        // 3.- ADD THE ITEM TO THE USER INTERFACE
+
+        // 4.- CALCULATE THE BUDGET
+
+        // 5.- DISPLAY THE BUDGET
     
-document.addEventListener('keypress', function(event){
-    
-    if(event.keyCode === 13 || event.which === 13) {
-        controlAddItem();
-        
-    }
+    }    
+
+    document.querySelector(DOM.inputButton).addEventListener('click', controlAddItem);
+
+    // GLOBAL DOCUMENT
+
+    document.addEventListener('keypress', function(event){
+
+        if(event.keyCode === 13 || event.which === 13) {
+            controlAddItem();
+
+        }
     
     
 });
